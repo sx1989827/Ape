5b516c1426d8d6ffa95041d01d7fde7279f9f94c
Geminy066 <you.remlove@163.com>
2016-03-28 18:17:48 +0800

    添加工程

---
+++ Client/Ape/Ape/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ape
+//
+//  Created by Geminy on 16/3/28.
+//  Copyright © 2016年 Geminy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
